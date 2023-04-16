cda31143a65cc33aea1b9f69af942628395d0deb
Gabriel Siriani <gabrielsiriani19@gmail.com>
2023-04-16 15:20:02 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
- 
+// Menu de Dropdown
+
+const app = { // Objeto que armazena nossas variáveis dentro do escopo fechado
+    $: {
+        menu: document.querySelector(".menu"),
+        menuItens: document.querySelector(".itens"),
+    },
+};
+
+app.$.menu.addEventListener("click", (event) => {
+    app.$.menuItens.classList.toggle("hidden");
+})
+//
