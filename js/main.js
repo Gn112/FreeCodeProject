244cd2da8ac3bc5e1fc8990bc87f723095e00398
Gabriel Siriani <gabrielsiriani19@gmail.com>
2023-04-18 20:47:54 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,9 +5,13 @@ const app = { // Objeto que armazena nossas variáveis dentro do escopo fechado
         menu: document.querySelector(".menu"),
         menuItens: document.querySelector(".itens"),
     },
-};
 
-app.$.menu.addEventListener("click", (event) => {
-    app.$.menuItens.classList.toggle("hidden");
-})
+    init() { // Podemos controlar quando o evento irá iniciar
+        app.$.menu.addEventListener("click", (event) => { //
+            app.$.menuItens.classList.toggle("hidden");
+        })
+    }
+}
+
+window.addEventListener("load", app.init); // gatilho para inicialização do evento
 //
